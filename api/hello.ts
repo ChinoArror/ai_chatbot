ae01dc11c3a2f9b9576e613b8b387fdf82cc4509
arrorchino <arror.rhy@outlook.com>
2025-07-28 23:54:24 +0800

    change

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
+// 需要先安装 @vercel/node 依赖
+// npm install @vercel/node --save
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({ message: 'Hello from API!' });
 }
